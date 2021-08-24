c43785105cf92432cbbe4e374edcc61353c23d56
JGDQKS <772163477sl@gmail.com>
2021-08-24 21:49:55 +1000

    Calculator page UI

--- FIT5120 - VicEnerG/App_Start/BundleConfig.cs
+++ FIT5120 - VicEnerG/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace FIT5120___VicEnerG
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css"
                       ));
+            bundles.Add(new ScriptBundle("~/bundles/chartjs").Include(
+              "~/Scripts/Chart.min.js"));
         }
     }
 }
