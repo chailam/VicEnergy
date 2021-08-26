d93810cda05437be2d463c108a1871da7f2600b6
Rongxuan Li <rlii0065@student.monash.edu>
2021-08-27 03:08:12 +0800

    Fix on the layout of few areas Reorganised the project files

--- FIT5120 - VicEnerG/App_Start/BundleConfig.cs
+++ FIT5120 - VicEnerG/App_Start/BundleConfig.cs
@@ -25,6 +25,55 @@ namespace FIT5120___VicEnerG
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css"
                       ));
+
+            bundles.Add(new StyleBundle("~/Home_Resource/css").Include(
+                      "~/Home_Resource/css/bootstrap.css",
+                      "~/Home_Resource/css/style.css",
+                      "~/Home_Resource/css/font-awesome.css",
+                      "~/Home_Resource/css/nav.css",
+                      "~/Home_Resource/css/smoothbox.css",
+                      "~/Home_Resource/css/flexslider.css"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/Home_Resource/js").Include(
+                      "~/Home_Resource/js/jquery-2.1.4.min.js",
+                      "~/Home_Resource/js/bootstrap.js",
+                      "~/Home_Resource/js/responsiveslides.min.js",
+                      "~/Home_Resource/js/jquery.flexslider.js",
+                      "~/Home_Resource/js/smoothbox.jquery2.js",
+                      "~/Home_Resource/js/move-top.js",
+                      "~/Home_Resource/js/easing.js",
+                      "~/Home_Resource/js/waypoints.min.js",
+                      "~/Home_Resource/js/counterup.min.js",
+                      "~/Home_Resource/js/classie.js",
+                      //"~/Home_Resource/js/nav.js",
+                      "~/Home_Resource/js/main.js",
+                      "~/Home_Resource/js/pagejs.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Pages_Resource/css").Include(
+                      "~/Pages_Resource/css/bootstrap.min.css",
+                      "~/Pages_Resource/css/style.css",
+                      "~/Pages_Resource/css/font-awesome.min.css",
+                      "~/Pages_Resource/css/lsb.css",
+                      "~/Pages_Resource/css/owl.carousel.css",
+                      "~/Pages_Resource/css/flexslider.css"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/Pages_Resource/js").Include(
+                      "~/Pages_Resource/js/bootstrap.min.js",
+                      "~/Pages_Resource/js/easing.js",
+                      "~/Pages_Resource/js/jquery-2.1.4.min.js",
+                      "~/Pages_Resource/js/jquery.flexisel.js",
+                      "~/Pages_Resource/js/jquery.flexslider.js",
+                      "~/Pages_Resource/js/lsb.min.js",
+                      "~/Pages_Resource/js/move-top.js",
+                      "~/Pages_Resource/js/owl.carousel.js",
+                      "~/Pages_Resource/js/SmoothScroll.min.js",
+                      "~/Pages_Resource/js/pagejs.js"
+                      ));
+
+
             bundles.Add(new ScriptBundle("~/bundles/chartjs").Include(
               "~/Scripts/Chart.min.js"));
         }
---
+++ FIT5120 - VicEnerG/Pages_Resource/js/pagejs.js
@@ -0,0 +1,66 @@
+﻿$(window).load(function () {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        start: function (slider) {
+            $('body').removeClass('loading');
+        }
+    });
+});
+$(document).ready(function () {
+    $("#owl-demo").owlCarousel({
+        items: 1,
+        lazyLoad: true,
+        autoPlay: true,
+        navigation: false,
+        navigationText: false,
+        pagination: true,
+    });
+});
+$(window).load(function () {
+    $("#flexiselDemo1").flexisel({
+        visibleItems: 4,
+        animationSpeed: 1000,
+        autoPlay: true,
+        autoPlaySpeed: 3000,
+        pauseOnHover: true,
+        enableResponsiveBreakpoints: true,
+        responsiveBreakpoints: {
+            portrait: {
+                changePoint: 480,
+                visibleItems: 1
+            },
+            landscape: {
+                changePoint: 640,
+                visibleItems: 2
+            },
+            tablet: {
+                changePoint: 768,
+                visibleItems: 2
+            }
+        }
+    });
+
+});
+$(window).load(function () {
+    $.fn.lightspeedBox();
+});
+$(document).ready(function () {
+    /*
+        var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear'
+        };
+    */
+
+    $().UItoTop({ easingType: 'easeOutQuart' });
+
+});
+jQuery(document).ready(function ($) {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    });
+});
+
