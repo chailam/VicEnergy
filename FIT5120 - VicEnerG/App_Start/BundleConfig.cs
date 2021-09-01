db906d95fcebf03163322fd9b9997c385d5f374e
Chai Lam <cloi0001@student.monash.edu>
2021-09-01 13:19:53 +1000

    remove the home-page-resource folder and update bundleconfig

--- FIT5120 - VicEnerG/App_Start/BundleConfig.cs
+++ FIT5120 - VicEnerG/App_Start/BundleConfig.cs
@@ -25,29 +25,6 @@ namespace FIT5120___VicEnerG
             bundles.Add(new StyleBundle("~/Style/css").Include(
                       "~/Style/bootstrap.css"
                       ));
-            bundles.Add(new StyleBundle("~/Home_Resource/css/homecss").Include(
-                      "~/Home_Resource/css/bootstrap.css",
-                      "~/Home_Resource/css/style.css",
-                      "~/Home_Resource/css/font-awesome.css",
-                      "~/Home_Resource/css/nav.css",
-                      "~/Home_Resource/css/smoothbox.css",
-                      "~/Home_Resource/css/flexslider.css"
-                      ));
-            bundles.Add(new ScriptBundle("~/Home_Resource/js/homejs").Include(
-                      "~/Home_Resource/js/jquery-2.1.4.min.js",
-                      "~/Home_Resource/js/bootstrap.js",
-                      "~/Home_Resource/js/responsiveslides.min.js",
-                      "~/Home_Resource/js/jquery.flexslider.js",
-                      "~/Home_Resource/js/smoothbox.jquery2.js",
-                      "~/Home_Resource/js/move-top.js",
-                      "~/Home_Resource/js/easing.js",
-                      "~/Home_Resource/js/waypoints.min.js",
-                      "~/Home_Resource/js/counterup.min.js",
-                      "~/Home_Resource/js/classie.js",
-                      //"~/Home_Resource/js/nav.js",
-                      "~/Home_Resource/js/main.js",
-                      "~/Home_Resource/js/pagejs.js"
-                      ));
 
             bundles.Add(new StyleBundle("~/Pages_Resource/css/pagecss").Include(
                       "~/Pages_Resource/css/bootstrap.min.css",
@@ -76,6 +53,5 @@ namespace FIT5120___VicEnerG
 
             BundleTable.EnableOptimizations = true;
         }
-       
     }
 }
