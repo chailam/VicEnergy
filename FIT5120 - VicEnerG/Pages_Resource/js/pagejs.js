cab18e99d2b268f20d38f9f3bbb6bf805fcf7474
Chai Lam <cloi0001@student.monash.edu>
2021-09-12 00:35:47 +1000

    dotted navigation bar for solar calculator page

--- FIT5120 - VicEnerG/Pages_Resource/js/pagejs.js
+++ FIT5120 - VicEnerG/Pages_Resource/js/pagejs.js
@@ -71,5 +71,52 @@ $(function () {
         var $nav = $(".navbar-fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
-}); 
+});
+
+
+
+/* Function to Solar Calculator Pge Dotted Navigation Bar */
+$(document).ready(function ($) {
+    var parPosition = [];
+    $('.par').each(function () {
+        parPosition.push($(this).offset().top+500);
+    });
+
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top-80
+        }, 500);
+        return false;
+    });
+
+    $('.vNav ul li a').click(function () {
+        $('.vNav ul li a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.vNav a').hover(function () {
+        $(this).find('.label').show();
+    }, function () {
+        $(this).find('.label').hide();
+    });
+
+    $(document).scroll(function () {
+        var position = $(document).scrollTop(),
+            index;
+        for (var i = 0; i < parPosition.length; i++) {
+            if (position <= parPosition[i]) {
+                index = i;
+                break;
+            }
+        }
+        $('.vNav ul li a').removeClass('active');
+        $('.vNav ul li a:eq(' + index + ')').addClass('active');
+    });
+
+    $('.vNav ul li a').click(function () {
+        $('.vNav ul li a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
 
